c461cad3208509651acfbffbe85a218d04747853
Fernel Augusto García Beltrán <fernel.garcia91@gmail.com>
2023-11-23 06:50:30 -0500

    Add files via upload
    Creación del proyecto

---
+++ admin_venta_casas/src/main/java/co/com/edu/admin_venta_casas/AdminVentaCasasApplication.java
@@ -0,0 +1,13 @@
+package co.com.edu.admin_venta_casas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminVentaCasasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminVentaCasasApplication.class, args);
+	}
+
+}
---
+++ admin_venta_casas/src/test/java/co/com/edu/admin_venta_casas/AdminVentaCasasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.edu.admin_venta_casas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminVentaCasasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
